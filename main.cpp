131ab2e1cf4075833c42b38502e1964b30c2f69f
AllFromBCN <soominjeong93@gmail.com>
2018-03-12 17:08:42 +0900

    multiple test case errors

--- main.cpp
+++ main.cpp
@@ -4,54 +4,33 @@
 using namespace std;
 #define DE 9
 
-bool abox[9][10];
-bool arow[9][10];
-bool acol[9][10];
-vector<int> options[9];
-
-void setMat(int mat[][9], int i, int j, int num, bool set)
-{
-    if (set)
-        mat[i][j] = num;
-    else mat[i][j] = 0;
-
-    arow[i][num]=set;
-    acol[j][num]=set;
-    abox[(i/3)*3+(j/3)][num]=set;
-}
-
 void showmat(int mat[][9])
 {
     for (int i=0; i<DE; i++)
     {
         for (int j=0; j<DE; j++)
-            cout << mat[i][j] ;
-        cout << endl;
+            cout << mat[i][j] <<" ";
     }
+    cout << endl;
 }
-bool checkSolution(int nthRow, int nthCol, int cand)
+bool checkSolution(int nthRow, int nthCol, int cand, bool abox[][10], bool acol[][10], bool arow[][10])
 {
-    if(!arow[nthRow][cand])
-        if(!acol[nthCol][cand])
-            if(!abox[(nthRow/3)*3+(nthCol/3)][cand])
-                return true;
+    //row
+    if(!arow[nthRow][cand] && !acol[nthCol][cand] && !abox[(nthRow/3)*3+(nthCol/3)][cand])
+        return true;
     return false;
 }
 
 bool checkEmptycell(int mat[][9], int &row, int &col)
 {
     for (row=0; row<DE; row++)
-    {
         for (col=0; col<DE; col++)
-        {
             if(mat[row][col] == 0)
                 return false;
-        }
-    }
     return true;
 }
 
-bool solve(int mat[9][9]) {
+bool solve(int mat[9][9], bool abox[][10], bool acol[][10], bool arow[][10], vector <int> options[]) {
     int row, col;
 
     if (checkEmptycell(mat, row, col))
@@ -61,22 +40,36 @@ bool solve(int mat[9][9]) {
     vector<int>::iterator it;
     for (it=options[row].begin(); it!=options[row].end(); ++it)
     {
-        if (checkSolution(row, col, *it))
+        if (checkSolution(row, col, *it, abox, acol, arow))
         {
-            setMat(mat, row, col, *it, true);
-            if (solve(mat))
+            mat[row][col] = *it;
+            abox[(row/3)*3+(col/3)][*it]=true;
+            arow[row][*it]=true;
+            acol[col][*it]=true;
+            if (solve(mat, abox, acol, arow, options))
                 return true;
-            setMat(mat, row, col, *it, false);
+            mat[row][col] = 0;
+            abox[(row/3)*3+(col/3)][*it]=false;
+            arow[row][*it]=false;
+            acol[col][*it]=false;
         }
     }
+    return false;
 }
 
 
 int main() {
-    int T, i, j, temp;
+    int T;
     cin >> T;
-    while (T--)
+    while(T--)
     {
+        int i, j, temp;
+        bool abox[9][10];
+        bool arow[9][10];
+        bool acol[9][10];
+        vector<int> emptySpace[9];
+        vector<int> options[9];
+
         int mat[DE][DE];
         for (i=0; i<DE; i++)
         {
@@ -93,7 +86,13 @@ int main() {
             for (j=0; j<DE; j++)
             {
                 cin >> temp;
-                setMat(mat,i,j,temp, true);
+                mat[i][j] = temp;
+                if(temp==0)
+                    emptySpace[i].push_back(j);
+
+                arow[i][temp]=true;
+                acol[j][temp]=true;
+                abox[(i/3)*3+(j/3)][temp]=true;
             }
         }
         for (i=0; i<DE; i++)
@@ -103,11 +102,11 @@ int main() {
                     options[i].push_back(j);
         }
 
-        solve(mat);
-        showmat(mat);
+        solve(mat, abox, acol, arow, options);
 
         for (i=0; i<DE; i++)
             options[i].clear();
+        showmat(mat);
     }
     return 0;
 }
